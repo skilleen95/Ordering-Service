765012ccee19ec866551cbf8819e7f63508548be
Killeen <s.killeen@accenture.com>
2021-03-19 13:14:57 -0400

    Cleaning up unused import

--- src/test/java/com/skilleen/firstmicroservice/FirstMicroserviceApplicationTests.java
+++ src/test/java/com/skilleen/firstmicroservice/FirstMicroserviceApplicationTests.java
@@ -1,7 +1,6 @@
 package com.skilleen.firstmicroservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
