476d2ecbda03373192c03da1db8427ac3a58d253
Killeen <s.killeen@accenture.com>
2021-04-15 11:48:19 -0400

    Adding consumer endpoint

--- src/main/java/com/skilleen/orderingservice/routes/KafkaRoutes.java
+++ src/main/java/com/skilleen/orderingservice/routes/KafkaRoutes.java
@@ -15,11 +15,11 @@ public class KafkaRoutes extends RouteBuilder {
                 .log("Recieved Message!")
                 .to("kafka:scotts-topic?brokers=172.30.74.234:9092");
 
-/*        from("kafka:scotts-topic?brokers=localhost:9092")
+        from("kafka:scotts-topic?brokers=172.30.74.234:9092")
                 .log("Message received from Kafka : ${body}")
                 .log("    on the topic ${headers[kafka.TOPIC]}")
                 .log("    on the partition ${headers[kafka.PARTITION]}")
                 .log("    with the offset ${headers[kafka.OFFSET]}")
-                .log("    with the key ${headers[kafka.KEY]}");*/
+                .log("    with the key ${headers[kafka.KEY]}");
     }
 }
