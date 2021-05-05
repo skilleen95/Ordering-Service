17fbf96d46c844dd13f93ef4d6ebd5dd4ae5c3b4
Killeen <s.killeen@accenture.com>
2021-05-05 15:50:35 -0400

    Adding NoArgs constructor

--- src/main/java/com/skilleen/orderingservice/entities/OrderEntity.java
+++ src/main/java/com/skilleen/orderingservice/entities/OrderEntity.java
@@ -2,6 +2,7 @@ package com.skilleen.orderingservice.entities;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import java.sql.Date;
 @Entity(name = "Order")
 @Data
 @Builder
+@NoArgsConstructor
 public class OrderEntity {
 
     @Id
