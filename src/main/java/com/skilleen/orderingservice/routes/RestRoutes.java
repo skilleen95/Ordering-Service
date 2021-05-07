ba8b1b89579c845f78a17fd0a640b39f363b3c3d
Killeen <s.killeen@accenture.com>
2021-05-07 13:01:34 -0400

    testing

--- src/main/java/com/skilleen/orderingservice/routes/RestRoutes.java
+++ src/main/java/com/skilleen/orderingservice/routes/RestRoutes.java
@@ -57,7 +57,7 @@ public class RestRoutes extends RouteBuilder {
                 .id("add-order2")
                 .saga()
                 .multicast()
-                .stopOnException().markRollbackOnlyLast().end()
+                .stopOnException().markRollbackOnlyLast()
                 .to("direct:insert-new-order")
                 .to("direct:create-shipping-request");
 
